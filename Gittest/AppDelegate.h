e735ccd543e510a6ca4bef65bcc1b9368fb1271f
taatata <taatatatati@gmail.com>
2015-11-26 19:04:47 +0900

    Initial Commit

---
+++ Gittest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gittest
+//
+//  Created by Daewoo Song on 2015/11/26.
+//  Copyright © 2015年 taro.kirameki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Gittest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Gittest
+//
+//  Created by Daewoo Song on 2015/11/26.
+//  Copyright © 2015年 taro.kirameki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
